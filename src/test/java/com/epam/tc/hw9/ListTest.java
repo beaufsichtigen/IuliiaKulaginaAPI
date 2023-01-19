95c0ffca8c6d97e6e52004c8fa0ef9a9546b42d1
“Iuliia <Iuliia_Kulagina@epam.com>
2023-01-19 23:11:01 +0100

    minimal works

--- src/test/java/com/epam/tc/hw9/ListTest.java
+++ src/test/java/com/epam/tc/hw9/ListTest.java
@@ -1,24 +1,11 @@
 package com.epam.tc.hw9;
 
-
-import static com.epam.tc.hw9.specs.board.CreateBoardSpecs.getRequestCreateBoardSuccess;
-import static com.epam.tc.hw9.specs.board.CreateBoardSpecs.getResponseCreateBoardSuccess;
-import static com.epam.tc.hw9.specs.board.DeleteBoardSpecs.getRequestDeleteBoardSuccess;
-import static com.epam.tc.hw9.specs.board.DeleteBoardSpecs.getResponseDeleteBoardSuccess;
-import static com.epam.tc.hw9.specs.board.GetBoardSpecs.getRequestGetBoardSuccess;
-import static com.epam.tc.hw9.specs.board.GetBoardSpecs.getResponseGetBoardSuccess;
-import static com.epam.tc.hw9.specs.board.GetBoardSpecs.getResponseGetBoardUnauth;
-import static com.epam.tc.hw9.specs.board.UpdateBoardSpecs.getRequestUpdateBoardSuccess;
-import static com.epam.tc.hw9.specs.board.UpdateBoardSpecs.getResponseUpdateBoardSuccess;
-import static com.epam.tc.hw9.specs.card.CreateCardSpecs.getRequestCreateCardSuccess;
-import static com.epam.tc.hw9.specs.card.CreateCardSpecs.getRequestGetCardSuccess;
-import static com.epam.tc.hw9.specs.card.CreateCardSpecs.getResponseCreateCardSuccess;
-import static com.epam.tc.hw9.specs.card.CreateCardSpecs.getResponseGetCardSuccess;
 import static com.epam.tc.hw9.specs.list.CreateListSpecs.getRequestCreateListSuccess;
+import static com.epam.tc.hw9.specs.list.CreateListSpecs.getRequestGetListSuccess;
 import static com.epam.tc.hw9.specs.list.CreateListSpecs.getResponseCreateListSuccess;
+import static com.epam.tc.hw9.specs.list.CreateListSpecs.getResponseGetListSuccess;
 import static io.restassured.RestAssured.given;
 
-import com.epam.tc.hw9.entities.Board;
 import org.testng.annotations.Test;
 
 public class ListTest extends BaseAPItest {
@@ -30,7 +17,7 @@ public class ListTest extends BaseAPItest {
     String newListPos = "top";
 
     @Test
-    public void createCardTest() {
+    public void createListTest() {
 
         var createListResponse = given()
             .spec(getRequestCreateListSuccess(key, token, newListName, newListPos))
@@ -42,18 +29,18 @@ public class ListTest extends BaseAPItest {
             .spec(getResponseCreateListSuccess(newListName, newListPos));
         createdListId = createListResponse.extract().body().path("id");
         createdListBody = createListResponse.extract().body();
-        System.out.println("cardID: " + createdListId);
+        System.out.println("createdListID: " + createdListId);
     }
 
-//    @Test
-//    public void getCardTest() {
-//        var getCardResponse = given()
-//            .spec(getRequestGetCardSuccess(key, token))
-//            .pathParam("id", createdCardId)
-//            .when()
-//            .get()
-//            .then()
-//            .spec(getResponseGetCardSuccess(newCardName)).extract().body().equals(createdCardBody);
-//    }
+    @Test
+    public void getListTest() {
+        var getListResponse = given()
+            .spec(getRequestGetListSuccess(key, token))
+            .pathParam("id", createdListId)
+            .when()
+            .get()
+            .then()
+        .spec(getResponseGetListSuccess(newListName, newListPos)).extract().body().equals(createdListBody);
+    }
 }
 
