9dd985bd00b0f3da8972d5192ef95ab52e391c35
“Iuliia <Iuliia_Kulagina@epam.com>
2023-01-24 09:22:24 +0100

    Minor changes

--- src/test/java/com/epam/tc/hw9/data/Data.java
+++ src/test/java/com/epam/tc/hw9/data/Data.java
@@ -7,13 +7,14 @@ import java.net.HttpURLConnection;
 import org.testng.annotations.DataProvider;
 
 public class Data {
-        @DataProvider(name = "boardErrors")
-        public static Object[][] sumDataLong() {
-            return new Object[][]{
-                //String key, String token, String boardId, int errorCode, String body
-                {"IncorrectKey", Auth.getToken(), boardId, HttpURLConnection.HTTP_UNAUTHORIZED, "invalid key"},
-                {Auth.getKey(), "IncorrectToken", boardId, HttpURLConnection.HTTP_UNAUTHORIZED, "invalid token"},
-                {Auth.getKey(), Auth.getToken(), "IncorrectId", HttpURLConnection.HTTP_BAD_REQUEST, "invalid id"}
-            };
-        }
+    @DataProvider(name = "boardErrors")
+    public static Object[][] sumDataLong() {
+        return new Object[][] {
+            //String key, String token, String boardId, int errorCode, String body
+            {"IncorrectKey", Auth.getToken(), boardId, HttpURLConnection.HTTP_UNAUTHORIZED, "invalid key"},
+            {Auth.getKey(), "IncorrectToken", boardId, HttpURLConnection.HTTP_UNAUTHORIZED, "invalid token"},
+            {"IncorrectKey", "IncorrectToken", boardId, HttpURLConnection.HTTP_UNAUTHORIZED, "invalid key"},
+            {Auth.getKey(), Auth.getToken(), "IncorrectId", HttpURLConnection.HTTP_BAD_REQUEST, "invalid id"}
+        };
+    }
 }
